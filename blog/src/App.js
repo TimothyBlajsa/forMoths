f35aeca60584ba83cd0200d91986d389eb628784
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-04 21:01:01 -0500

    Import components
    Create component files

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
+import Title from './Title';
+import Body from './Body';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Porchlight #1
       </header>
     </div>
   );
