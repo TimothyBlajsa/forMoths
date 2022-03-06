cdbcb865c5c8ae0a4aba5d2ea3178ee2eba644b5
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-06 11:00:04 -0500

    Add accordion component

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,6 +3,12 @@ import Title from './Title';
 import Body from './Body';
 import Footer from './Footer';
 import './App.css';
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function App() {
   return (
@@ -10,7 +16,24 @@ function App() {
       <header className="App-header">
        Porchlight #1
        <p>Ƹ̵̡Ӝ̵̨̄Ʒ Ƹ̵̡Ӝ̵̨̄Ʒ Ƹ̵̡Ӝ̵̨̄Ʒ</p>
+       <div className="container">
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panella-content"
+          id="panella-header"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              This is the sample text for the accordion
+            </Typography>
+          </AccordionDetails>
+      </Accordion>
+    </div>
       </header>
+    
     </div>
   );
 }
